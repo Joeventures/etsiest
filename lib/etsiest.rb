126e9829cdc7357705e38ec2d15a727d0eb71115
Joe Winter <joe@joeventures.com>
2015-10-23 12:34:15 -0400

    Sinatra begins his set. Fly me to the moon.

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,12 +1,19 @@
 require "etsiest/version"
 require "pry"
 require "etsiest/etsysearch"
+require "sinatra/base"
 
 require 'etsy'
 Etsy.api_key = ENV['ETSY_KEY']
 
 module Etsiest
-  class App
-    binding.pry
+  class App < Sinatra::Base
+    set :logging, true
+
+    get "/" do
+      erb :index
+    end
+
+    run! if app_file == $0
   end
 end
